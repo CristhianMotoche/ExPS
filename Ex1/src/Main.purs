9a8be5bea6bea4ba2640f6664769fe2a649b58ed
CristhianMotoche <cristhian.motoche@gmail.com>
2016-03-06 18:34:43 -0500

    Add treeTo*

--- Ex1/src/Main.purs
+++ Ex1/src/Main.purs
@@ -1,6 +1,7 @@
 module Main where
 
 import Prelude
+import Data.List
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
@@ -10,12 +11,22 @@ countLeaves :: forall typ. Tree typ -> Int
 countLeaves Empty = 0
 countLeaves (Leaf l _ r) = 1 + countLeaves l + countLeaves r
 
+treeToList :: forall typ. Tree typ -> List typ
+treeToList Empty = Nil
+treeToList (Leaf Empty x Empty) = Cons x (Nil)
+treeToList (Leaf l x r) = treeToList l ++ Cons x (Nil) ++ treeToList r
+
+treeToArray :: forall typ. Tree typ -> Array typ
+treeToArray Empty = []
+treeToArray (Leaf Empty x Empty) = [x]
+treeToArray (Leaf l x r) = treeToArray l ++ [ x ] ++ treeToArray r
+
 instance showTree :: (Show a) => Show (Tree a) where
     show (Leaf l x r) =
         "T: " ++ show x ++ "(" ++ (show l) ++ ")" ++ "(" ++ (show r) ++ ")"
     show Empty = "-"
 
-main :: forall t. Eff (console :: CONSOLE | t) Unit
+main :: forall t. Eff ( console :: CONSOLE | t ) Unit
 main = do
       log "TREE: "
       log $ show tree
