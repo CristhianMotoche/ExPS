23d369b217ad5c0d05b5c6eac83c51235bb500e9
CristhianMotoche <cristhian.motoche@gmail.com>
2016-03-06 19:40:39 -0500

    Separate Tree functions

--- Ex1/src/Main.purs
+++ Ex1/src/Main.purs
@@ -1,30 +1,9 @@
 module Main where
 
-import Prelude
-import Data.List
+import Prelude (Unit, show, ($), bind)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-
-data Tree a = Leaf (Tree a) a (Tree a) | Empty
-
-countLeaves :: forall typ. Tree typ -> Int
-countLeaves Empty = 0
-countLeaves (Leaf l _ r) = 1 + countLeaves l + countLeaves r
-
-treeToList :: forall typ. Tree typ -> List typ
-treeToList Empty = Nil
-treeToList (Leaf Empty x Empty) = Cons x (Nil)
-treeToList (Leaf l x r) = treeToList l ++ Cons x (Nil) ++ treeToList r
-
-treeToArray :: forall typ. Tree typ -> Array typ
-treeToArray Empty = []
-treeToArray (Leaf Empty x Empty) = [x]
-treeToArray (Leaf l x r) = treeToArray l ++ [ x ] ++ treeToArray r
-
-instance showTree :: (Show a) => Show (Tree a) where
-    show (Leaf l x r) =
-        "T: " ++ show x ++ "(" ++ (show l) ++ ")" ++ "(" ++ (show r) ++ ")"
-    show Empty = "-"
+import Tree
 
 main :: forall t. Eff ( console :: CONSOLE | t ) Unit
 main = do
---
+++ Ex1/src/Tree.purs
@@ -0,0 +1,34 @@
+module Tree
+    ( createIntTree
+    , countLeaves
+    , treeToList
+    , treeToArray
+    , Tree(..))
+    where
+
+import Prelude (class Show, show, (++), (+))
+import Data.List (List(Nil, Cons))
+
+data Tree a = Leaf (Tree a) a (Tree a) | Empty
+
+createIntTree :: Int -> Tree Int
+createIntTree x = Leaf Empty x Empty
+
+countLeaves :: forall typ. Tree typ -> Int
+countLeaves Empty = 0
+countLeaves (Leaf l _ r) = 1 + countLeaves l + countLeaves r
+
+treeToList :: forall typ. Tree typ -> List typ
+treeToList Empty = Nil
+treeToList (Leaf Empty x Empty) = Cons x (Nil)
+treeToList (Leaf l x r) = treeToList l ++ Cons x (Nil) ++ treeToList r
+
+treeToArray :: forall typ. Tree typ -> Array typ
+treeToArray Empty = []
+treeToArray (Leaf Empty x Empty) = [x]
+treeToArray (Leaf l x r) = treeToArray l ++ [ x ] ++ treeToArray r
+
+instance showTree :: (Show a) => Show (Tree a) where
+    show (Leaf l x r) =
+        "T: " ++ show x ++ "(" ++ (show l) ++ ")" ++ "(" ++ (show r) ++ ")"
+    show Empty = "-"
